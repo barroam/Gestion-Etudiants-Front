47170c236f7dbc5e33aebfe6806ce82de3e20d7a
barroam <barroama23@gail.com>
2024-10-30 15:24:33 +0000

    ajout de la gestions des matieres , UEs , evaluations, et le tableau de bord

--- src/app/Composant/Evaluation/list-evaluation/list-evaluation.component.ts
+++ src/app/Composant/Evaluation/list-evaluation/list-evaluation.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EvaluationsService } from '../../../Services/Evaluations/evaluations.service';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-evaluation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './list-evaluation.component.html',
   styleUrl: './list-evaluation.component.css'
 })
-export class ListEvaluationComponent {
+export class ListEvaluationComponent   implements OnInit {
+  evaluations: any[] = [];
 
+  constructor(private evaluationsService: EvaluationsService ,
+    private router : Router) {}
+
+  ngOnInit(): void {
+    this.loadEvaluations();
+  }
+
+  loadEvaluations() {
+    this.evaluationsService.getEvaluations().subscribe((response) => {
+      this.evaluations = response.data;
+    //  console.log(response.data);
+
+    });
+  }
+
+
+  editEvaluation(id: number): void {
+    this.router.navigate([`/Dashbord-evaluations/add-update/${id}`]);
+  }
+
+  deleteEvaluation(id: number) {
+    this.evaluationsService.deleteEvaluation(id).subscribe(() => {
+      this.loadEvaluations();
+    });
+  }
 }
--- src/app/Composant/Matiere/add-update-matiere/add-update-matiere.component.ts
+++ src/app/Composant/Matiere/add-update-matiere/add-update-matiere.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatieresService } from '../../../Services/Matieres/matieres.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { response } from 'express';
 
 @Component({
   selector: 'app-add-update-matiere',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-update-matiere.component.html',
   styleUrl: './add-update-matiere.component.css'
 })
-export class AddUpdateMatiereComponent {
+export class AddUpdateMatiereComponent implements OnInit {
+  matiereForm: FormGroup;
+  isEditMode = false;
+  matiereId: number | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private matieresService: MatieresService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.matiereForm = this.fb.group({
+      libelle: ['', [Validators.required, Validators.maxLength(255)]],
+      date_debut: ['', Validators.required],
+      date_fin: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.matiereId = +params['id'];
+      if (this.matiereId) {
+        this.isEditMode = true;
+        this.matieresService.getMatiereById(this.matiereId).subscribe(response => {
+          this.matiereForm.patchValue(response.data);
+        });
+      }
+    });
+  }
+
+  onSubmit() {
+    if (this.matiereForm.valid) {
+      if (this.isEditMode) {
+        this.matieresService.updateMatiere(this.matiereId!, this.matiereForm.value).subscribe(() => {
+          this.router.navigate(['/Dashbord-matieres/liste']);
+        });
+      } else {
+        this.matieresService.createMatiere(this.matiereForm.value).subscribe(() => {
+          this.router.navigate(['/Dashbord-matieres/liste']);
+        });
+      }
+    }
+  }
 }
